496480981d12224fd77e2e70c89c03eb9ad53c28
Grayson Miller <grayson.miller124@gmail.com>
2023-08-20 21:29:04 -0600

    visualize

--- mnister/visualize.py
+++ mnister/visualize.py
@@ -5,6 +5,7 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 from tensorflow.keras.datasets import mnist
 import numpy as np
+from matplotlib.animation import FuncAnimation
 
 # Load and preprocess the MNIST dataset
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
@@ -27,10 +28,8 @@ def get_saliency_map(model, image, true_class):
 
 
 def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None):
-    # Ensure the img_array is 4D
-    img_array = tf.expand_dims(img_array, 0)
-    img_array = tf.expand_dims(img_array, -1)
-
+    if len(img_array.shape) != 4:
+        img_array = tf.expand_dims(img_array, 0)
     grad_model = tf.keras.models.Model(
         [model.inputs], [model.get_layer(last_conv_layer_name).output, model.output]
     )
@@ -53,24 +52,46 @@ def make_gradcam_heatmap(img_array, model, last_conv_layer_name, pred_index=None
     return heatmap.numpy()
 
 
-def plot_digit_gallery(image, true_class, model):
-    saliency = get_saliency_map(model, np.expand_dims(image, axis=0), true_class)
+def integrated_visualization(saliency, grad_cam):
+    # Normalize both visualizations to [0, 1]
+    saliency = (saliency - np.min(saliency)) / (np.max(saliency) - np.min(saliency))
+    grad_cam = (grad_cam - np.min(grad_cam)) / (np.max(grad_cam) - np.min(grad_cam))
 
-    # prediction
-    prediction = model.predict(np.expand_dims(image, axis=0))
+    # Integrate
+    integrated = (saliency + grad_cam) / 2.0
+    return integrated
 
-    # grad-cam
-    grad = make_gradcam_heatmap(image, model, "conv2d_1")
-    grad = cv2.resize(grad, (28, 28))
 
-    return image, saliency, grad
+def update(frame, axes, model):
+    for digit in range(10):
+        index_list = np.where(test_labels == digit)[0]
+        index = index_list[frame % len(index_list)]
+        image = test_images[index]
+        true_class = np.argmax(test_labels[index])
+
+        saliency = get_saliency_map(model, np.expand_dims(image, axis=0), true_class)
+        grad_cam = make_gradcam_heatmap(image, model, "conv2d_1")
+
+        axes[digit, 0].imshow(image.squeeze(), cmap="gray")
+        axes[digit, 0].imshow(saliency.squeeze(), cmap="hot", alpha=0.75)
+        # axes[digit, 0].set_title("Saliency Map")
+        axes[digit, 0].axis("off")
+
+        axes[digit, 1].imshow(image.squeeze(), cmap="gray")
+        grad_cam = cv2.resize(grad_cam, (28, 28))  # grad_cam.squeeze()
+        axes[digit, 1].imshow(grad_cam, cmap="hot", alpha=0.75)
+        # axes[digit, 1].set_title("Grad-CAM")
+        axes[digit, 1].axis("off")
+
+        integrated_vis = integrated_visualization(saliency.squeeze(), grad_cam)
+        axes[digit, 2].imshow(image.squeeze(), cmap="gray")
+        axes[digit, 2].imshow(integrated_vis, cmap="hot", alpha=0.75)
+        axes[digit, 2].axis("off")
+
+    plt.tight_layout()
 
 
 def main():
-    # Choose an image from the test dataset
-    image_index = 0
-    image = test_images[image_index]
-
     # load model
     if Path("mnist_classifier.keras").exists():
         model = tf.keras.models.load_model("mnist_classifier.keras")
@@ -78,29 +99,13 @@ def main():
         raise Exception("No model found, please run __main__.py and train one first.")
 
     # Create a grid of 10x3 to plot the images
-    fig, axes = plt.subplots(10, 3, figsize=(15, 50))
+    fig, axes = plt.subplots(10, 3)
 
-    for digit in range(10):
-        index = np.where(test_labels == digit)[0][random.randint(0, 974)]
-        image = test_images[index]
-        true_class = np.argmax(test_labels[index])
+    ani = FuncAnimation(
+        fig, lambda f: update(f, axes=axes, model=model), frames=60, repeat=True
+    )  # 974 total frames possible
+    # ani.save("mnist.gif", writer="imagemagick", fps=8)
 
-        orig, saliency, grad_cam = plot_digit_gallery(image, true_class, model)
-
-        axes[digit, 0].imshow(orig.squeeze(), cmap="gray")
-        axes[digit, 0].set_title(f"Original Image: {digit}")
-        axes[digit, 0].axis("off")
-
-        axes[digit, 1].imshow(saliency.squeeze(), cmap="hot")
-        axes[digit, 1].set_title("Saliency Map")
-        axes[digit, 1].axis("off")
-
-        axes[digit, 2].imshow(orig.squeeze(), cmap="gray")
-        axes[digit, 2].imshow(grad_cam, cmap="hot", alpha=0.75)
-        axes[digit, 2].set_title("Grad-CAM")
-        axes[digit, 2].axis("off")
-
-    plt.tight_layout()
     plt.show()
 
 
